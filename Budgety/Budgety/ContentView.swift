1b8358a9e32cecea31b1d00815cff9d463d83a6c
Matthew Rampey <matthewrampey@tlcdelivers.com>
2020-03-19 14:33:03 -0400

    Initial commit creating the base structure of an IOS app.

---
+++ Budgety/Budgety/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Budgety
+//
+//  Created by Matthew Rampey on 3/19/20.
+//  Copyright © 2020 Matthew Rampey. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
